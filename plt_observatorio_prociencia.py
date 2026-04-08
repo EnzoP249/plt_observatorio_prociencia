0aa6a5d011f23068a57d62fe54cd36e0d708ce56
EnzoPeña <enzo.lpge@gmail.com>
2026-04-07 20:12:47 -0500

    Inicialización del proyecto

---
+++ plt_observatorio_prociencia.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 19:59:05 2026
+
+@author: Enzo
+"""
+
+###############################################################################
+## PROYECTO PARA ANÁLISIS DE DATOS E INFORMACIÓN DEL OBSERVATORIO DE
+## SUBVENCIONES DEL PROCIENCIA
+###############################################################################
+
+# El proyecto sigue un enfoque de librerias integradas
+
+
